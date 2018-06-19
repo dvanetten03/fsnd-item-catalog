42f8f7e6173788d11687b4b07a6875725ed5b992
vagrant <vagrant@vagrant.vm>
2018-06-19 04:08:59 +0000

    Add templates to routes on application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 # from sqlalchemy import create_engine
 # from sqlalchemy.orm import sessionmaker
 
@@ -15,43 +15,51 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/catalog')
 def showCatalog():
-	return "This page will show all catalog categories"
+	#return "This page will show all catalog categories"
+	return render_template('showcatalog.html')
 
 # Add new catalog category
 @app.route('/catalog/new', methods=['GET', 'POST'])
 def newCategory():
-	return "This page will be for making a new category"
+	#return "This page will be for making a new category"
+	return render_template('newcategory.html')
 
 # Edit catalog category
 @app.route('/catalog/<int:category_id>/edit', methods = ['GET', 'POST'])
 def editCategory():
-	return "This page will be for editing catalog category %s" %category_id
+	#return "This page will be for editing catalog category %s" %category_id
+	return render_template('editcategory.html')
 
 # Delete catalog category
 @app.route('/catalog/<int:category_id>/delete', methods = ['GET', 'POST'])
 def deleteCategory():
-	return "This page will be for deleting category %s" %category_id
+	#return "This page will be for deleting category %s" %category_id
+	return render_template('deletecategory.html')
 
 # Show a category's items
 @app.route('/catalog/<int:category_id>')
 @app.route('/catalog/<int:category_id>/items')
 def showItems():
-	return "This page is the item list for category %s" %category_id
+	#return "This page is the item list for category %s" %category_id
+	return render_template('showitems.html')
 
 # Make a new item for a category
 @app.route('/catalog/<int:category_id>/item/new', methods = ['GET', 'POST'])
 def newCategoryItem():
-	return "This page is for making a new item for a category %s" %category_id
+	#return "This page is for making a new item for a category %s" %category_id
+	return render_template('newcategoryitem.html')
 
 # Edit a category item
 @app.route('/catalog/<int:category_id>/item/<int:item_id>/edit')
 def editCategoryItem():
-	return "This page is for editing category item %s" %item_id
+	#return "This page is for editing category item %s" %item_id
+	return render_template('editcategoryitem.html')
 
 # Delete a category item
 @app.route('/catalog/<int:category_id>/item/<int:item_id>/delete')
 def deleteCategoryItem():
-	return "This page is for deleting category item %s" %item_id
+	#return "This page is for deleting category item %s" %item_id
+	return render_template('deletecategoryitem.html')
 
 
 if __name__ == '__main__':
