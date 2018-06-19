30963665f3f113760cae8b20fbadc74f48bd8e6f
vagrant <vagrant@vagrant.vm>
2018-06-19 20:43:31 +0000

    Add database_setup.py and begin temporary catalog to run some tests

---
+++ database_setup.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+	__tablename__ = 'user'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	email = Column(String(250), nullable=False)
+	picture = Column(String(250))
+
+class Catalog(Base):
+	__tablename__ = 'catalog'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
+
+	@property
+	def serialize(self):
+		return{
+		'name'		:self.name,
+		'id'		:self.id,
+		'user_id'	:self.user.id
+		}
+
+class Category(Base):
+	__tablename__ = 'category'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(80), nullable=False)
+	description = Column(String(250))
+	user_id = Column(Integer, ForeignKey('user.id'))
+	catalog = relationship(Catalog)
+	user = relationship(User)
+
+	@property
+	def serialize(self):
+		return{
+		'name'			:self.name,
+		'description'	:self.description,
+		'id'			:self.id
+		}
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
+	
