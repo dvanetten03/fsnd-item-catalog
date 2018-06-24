980a2cb2b82fee5e977c447a1e74d5cb9d5d32c0
vagrant <vagrant@vagrant.vm>
2018-06-24 03:08:03 +0000

    Begin Facebook login

--- application.py
+++ application.py
@@ -152,6 +152,44 @@ def gdisconnect():
 		response.headers['Content-Type'] = 'application/json'
 		return response
 
+#Add facebook login api
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+	if request.args.get('state') != login_session['state']:
+		response = make_response(json.dumps('Invalid state parameter.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	access_token = request.data
+	print ("access token received %s") %access_token
+
+	# Exchange client token for long-lived server-side token with Get /oauth/access_token?grant_type=fb_exchange_token&client_id={app-id}&fb_exchange_token={short-lived-token}
+	app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+	app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+	url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' %(app_id, app_secret, access_token)
+	h = httplib2.Http()
+	result = h.request(url, 'GET')[1]
+
+	# Use token to get user info from API
+	userinfo_url = "https://graph.facebook.com/v2.8/me"
+	#strip expire tag from access token
+	token = result.split(',')[0].split(':')[1].replace('"', '')
+
+	url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name, id, email' % token
+	h = httplib2.Http()
+	result = h.request(url, 'GET')[1]
+
+	data = json.loads(result)
+
+	login_session['provider'] = 'facebook'
+	login_session['username'] = data['name']
+	login_session['email'] = data['email']
+	login_session['facebook_id'] = data['id']
+
+	login_session['access_token'] = token
+
+	#Get user picture
+	url = "https://graph.facebook.com/v2.8/me/picture?%s&redirect=0&height=200&width=200" % token
+
 # Show all catalog categories
 @app.route('/')
 @app.route('/catalog')
